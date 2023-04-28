b946280525911d7b3e6921193899e79e1446cffe
myOmikron <git@omikron.dev>
2023-04-28 03:30:16 +0200

    Added reactjs-popup dependency

--- frontend/src/views/login.tsx
+++ frontend/src/views/login.tsx
@@ -1,16 +1,41 @@
 import React from "react";
+import Input from "../components/input.tsx";
 
 type LoginProps = {};
-type LoginState = {};
+type LoginState = {
+    username: string;
+    password: string;
+};
 
 export default class Login extends React.Component<LoginProps, LoginState> {
     constructor(props: LoginProps) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            username: "",
+            password: "",
+        };
     }
 
     render() {
-        return <div></div>;
+        return (
+            <div className={"login-container"}>
+                <div className={"login"}>
+                    <Input
+                        value={this.state.username}
+                        onChange={(v: string) => {
+                            this.setState({ username: v });
+                        }}
+                    />
+                    <Input
+                        value={this.state.password}
+                        onChange={(v: string) => {
+                            this.setState({ password: v });
+                        }}
+                        type={"password"}
+                    />
+                </div>
+            </div>
+        );
     }
 }

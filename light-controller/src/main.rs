414e6abcde19f021338b5de014711362438fd52e
myOmikron <git@omikron.dev>
2023-05-08 03:21:04 +0200

    Added rust project for esp32c3

---
+++ light-controller/src/main.rs
@@ -0,0 +1,10 @@
+use esp_idf_svc::http::server::ws::EspHttpWsConnection;
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_sys::link_patches();
+
+    let mut client = EspHttpWsConnection {};
+}
